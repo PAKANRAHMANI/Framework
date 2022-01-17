f124dc0ede35003e156fd3d0c9e629e5108ced4e
Ali Radravesh <aliradcharisma@gmail.com>
2022-01-17 13:41:02 +0330

    #1306 Add new overload for send method wuthout "Priority" parameter. Apply some refactoring for clena coding.

--- Code/Src/Framework.EasyNetQ/EasyNetQMessageSender.cs
+++ Code/Src/Framework.EasyNetQ/EasyNetQMessageSender.cs
@@ -4,6 +4,7 @@ using System.Text;
 using System.Threading.Tasks;
 using EasyNetQ;
 using EasyNetQ.Topology;
+using Framework.EasyNetQ.Helpers;
 using Framework.Messages;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
@@ -11,49 +12,68 @@ using IMessage = Framework.Messages.IMessage;
 
 namespace Framework.EasyNetQ
 {
-	public class EasyNetQMessageSender : IMessageSender
-	{
-		private readonly IBus _bus;
-		public EasyNetQMessageSender(IBus bus)
-		{
-			this._bus = bus;
-		}
+    public class EasyNetQMessageSender : IMessageSender
+    {
+        private readonly IBus _bus;
+        public EasyNetQMessageSender(IBus bus)
+        {
+            this._bus = bus;
+        }
 
-		public async Task Send(IMessage message, string queueName, Priority priority)
-		{
-			var messageContext = new MessageContext
-			{
-				CorrelationId = Guid.NewGuid().ToString(),
-				MessageType = BaseEventTypes.TypeNames,
-				Message = message
-			};
+        public async Task Send(IMessage message, string queueName, Priority priority)
+        {
+            var jsonBody = SetupMessage(message);
 
-			var jsonSerializerSettings = new JsonSerializerSettings
-			{
-				Formatting = Formatting.Indented,
-				ContractResolver = new CamelCasePropertyNamesContractResolver()
-			};
+            await _bus.Advanced.PublishAsync(
+                new Exchange(queueName),
+                "",
+                false,
+               messageProperties: new MessageProperties
+               {
+                   Priority = (byte)priority,
+               },
+                Encoding.UTF8.GetBytes(jsonBody)
+            );
+        }
 
-			var jsonBody = JsonConvert.SerializeObject(messageContext, jsonSerializerSettings);
+        public async Task Send(IMessage message, string queueName)
+        {
+            var jsonBody = SetupMessage(message);
 
-			await _bus.Advanced.PublishAsync(
-				new Exchange(queueName),
-				"",
-				false,
-				new MessageProperties
-				{
-					Priority = (byte)priority,
-				},
-				Encoding.UTF8.GetBytes(jsonBody)
-			);
-		}
+            await _bus.Advanced.PublishAsync(
+                new Exchange(queueName),
+                "",
+                false,
+                messageProperties: new MessageProperties
+                {
 
-		public async Task SendBatch(IEnumerable<IMessage> messages, string queueName, Priority priority)
-		{
-			foreach (var message in messages)
-			{
-				await this.Send(message, queueName, priority);
-			}
-		}
-	}
+                },
+                Encoding.UTF8.GetBytes(jsonBody)
+            );
+        }
+
+        private static string SetupMessage(IMessage message)
+        {
+            var messageContext = EasyNetQHelpers.CreateMessageContext(
+                Guid.NewGuid().ToString(),
+                messageTypes: BaseEventTypes.TypeNames,
+                 message: message);
+
+            var jsonSerializerSettings = EasyNetQHelpers.CreateJsonSerializerSettings(
+               formatting: Formatting.Indented,
+               contractResolver: new CamelCasePropertyNamesContractResolver());
+
+            var jsonBody = JsonConvert.SerializeObject(messageContext, jsonSerializerSettings);
+
+            return jsonBody;
+        }
+
+        public async Task SendBatch(IEnumerable<IMessage> messages, string queueName, Priority priority)
+        {
+            foreach (var message in messages)
+            {
+                await this.Send(message, queueName, priority);
+            }
+        }
+    }
 }
--- Code/Src/Framework.Messages/IMessageSender.cs
+++ Code/Src/Framework.Messages/IMessageSender.cs
@@ -6,6 +6,7 @@ namespace Framework.Messages
 	public interface IMessageSender
 	{
 		public Task Send(IMessage message, string queueName, Priority priority);
-		public Task SendBatch(IEnumerable<IMessage> messages, string queueName, Priority priority);
+		public Task Send(IMessage message, string queueName);
+        public Task SendBatch(IEnumerable<IMessage> messages, string queueName, Priority priority);
 	}
 }
