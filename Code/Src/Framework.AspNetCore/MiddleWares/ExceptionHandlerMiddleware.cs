07b7f09bd90be580b07b985c6423f411d44bd9ce
Ali Radravesh <aliradcharisma@gmail.com>
2022-01-19 14:24:24 +0330

    #1308 Install following nugets for working with "Sentry" "Sentry.AspNetCore" Version="2.1.8" "Sentry" Version="2.1.8" . Adding SentryConfiguration . Use SentrySdk for capturing UnhandledException in the ExceptionHandlerMiddleware.

--- Code/Src/Framework.AspNetCore/MiddleWares/ExceptionHandlerMiddleware.cs
+++ Code/Src/Framework.AspNetCore/MiddleWares/ExceptionHandlerMiddleware.cs
@@ -3,17 +3,32 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Newtonsoft.Json;
 using Framework.Core.Exceptions;
+using Framework.AspNetCore.Configurations;
+using Sentry;
 
 namespace Framework.AspNetCore.MiddleWares
 {
     public class ExceptionHandlerMiddleware
     {
+        #region Fields
+
         private readonly RequestDelegate _next;
 
-        public ExceptionHandlerMiddleware(RequestDelegate next)
+        private readonly SentryConfiguration _sentryConfiguration;
+
+        #endregion
+
+        #region Constructors
+
+        public ExceptionHandlerMiddleware(RequestDelegate next, SentryConfiguration sentryConfiguration)
         {
             _next = next;
+            _sentryConfiguration = sentryConfiguration;
         }
+
+        #endregion
+
+        #region Methods
 
         public async Task InvokeAsync(HttpContext context)
         {
@@ -23,11 +38,11 @@ namespace Framework.AspNetCore.MiddleWares
             }
             catch (Exception exception)
             {
-                await HandleException(context,exception);
+                await HandleException(context, exception);
             }
         }
 
-        private async Task HandleException(HttpContext context,Exception exception)
+        private async Task HandleException(HttpContext context, Exception exception)
         {
             if (exception is BusinessException businessException)
                 await HandleBusinessException(context, businessException);
@@ -40,16 +55,29 @@ namespace Framework.AspNetCore.MiddleWares
             await WriteExceptionToResponse(context, error);
         }
 
-        private async Task WriteExceptionToResponse(HttpContext context,ExceptionDetails error)
+        private async Task WriteExceptionToResponse(HttpContext context, ExceptionDetails error)
         {
             context.Response.StatusCode = (int)error.Code;
             context.Response.ContentType = "application/json";
             await context.Response.WriteAsync(JsonConvert.SerializeObject(error));
         }
+
         private async Task UnhandledException(HttpContext httpContext, Exception exception)
         {
-            var error =  ExceptionDetails.Create(exception.Message, -1000);
+            CaptureOnSentry(exception);
+
+            var error = ExceptionDetails.Create(exception.Message, -1000);
             await WriteExceptionToResponse(httpContext, error);
         }
+
+        private void CaptureOnSentry(Exception exception)
+        {
+            using (SentrySdk.Init(_sentryConfiguration.Dsn))
+            {
+                SentrySdk.CaptureException(exception);
+            }
+        }
+
+        #endregion
     }
 }
