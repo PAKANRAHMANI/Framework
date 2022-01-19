2add776076093bd17548cb8155dafcf91d43ba0f
Mohammad Hasan Rashidi <M.Rashidi@siteb.local>
2022-01-19 14:37:38 +0330

    #1332 add file upload module

---
+++ Code/Src/Framework.FileUpload/FileUploadModule.cs
@@ -0,0 +1,35 @@
+﻿using System.Net;
+using FluentFTP;
+using Framework.Config;
+
+
+namespace Framework.FileUpload
+{
+    public class FileUploadModule : IFrameworkModule
+    {
+        private readonly FtpServerConfiguration _ftpServerConfiguration;
+
+        public FileUploadModule(FtpServerConfiguration ftpServerConfiguration)
+        {
+            _ftpServerConfiguration = ftpServerConfiguration;
+        }
+
+        public void Register(IDependencyRegister dependencyRegister)
+        {
+            dependencyRegister.RegisterScoped<IPersistFile, FtpPersist>();
+            dependencyRegister.RegisterScoped<IFtpClient>(CreateFtpClient);
+        }
+
+        private FtpClient CreateFtpClient()
+        {
+            var client = new FtpClient($"{_ftpServerConfiguration.EndPoint}")
+            {
+                Credentials = new NetworkCredential(_ftpServerConfiguration.UserName, _ftpServerConfiguration.Password)
+            };
+
+            client.Connect();
+
+            return client;
+        }
+    }
+}
