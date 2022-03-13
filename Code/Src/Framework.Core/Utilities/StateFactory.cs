8de22197d5dc88ce79c4688f23e885f436c466b2
p.rahmani <p.rahmani@SITEB-PC57>
2022-03-13 10:38:19 +0330

    change statefactory to static

--- Code/Src/Framework.Core/Utilities/StateFactory.cs
+++ Code/Src/Framework.Core/Utilities/StateFactory.cs
@@ -6,23 +6,33 @@ using System.Reflection;
 
 namespace Framework.Core.Utilities
 {
-    public class StateFactory<T> where T : class
+    public static class StateFactory<T>
     {
-        private List<T> _states = new List<T>();
-        public StateFactory(List<T> states)
+        static StateFactory()
         {
-            states.ForEach(state => _states.Add(state));
+            States.Clear();
         }
-        public T Create(long state)
+        private static readonly List<T> States = new();
+
+        public static T Create(long state)
         {
-            return _states.First(a => a.GetType()
+            return States.First(a => a.GetType()
                 .GetCustomAttributes(typeof(StateValue), true)
                 .OfType<StateValue>().First().Value == state);
         }
 
-        public long Create(T state)
+        public static long Create(T state)
         {
             return state.GetType().GetCustomAttributes<StateValue>().First().Value;
+        }
+
+        public static void AddStates(params T[] states)
+        {
+            States.AddRange(states);
+        }
+        public static void ClearStates()
+        {
+            States.Clear();
         }
     }
 }
