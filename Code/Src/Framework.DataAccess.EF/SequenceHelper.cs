43d72d3d0ac1a3a4f94d4b45aa448cd129bb3b19
pakan rahmani <pakan.rahmani@gmail.com>
2022-07-14 22:00:09 +0430

    fix bug sequence number

--- Code/Src/Framework.DataAccess.EF/SequenceHelper.cs
+++ Code/Src/Framework.DataAccess.EF/SequenceHelper.cs
@@ -19,12 +19,12 @@ namespace Framework.DataAccess.EF
         }
         public async Task<long> Next(string sequenceName)
         {
-            var sequenceSqlParameter = new SqlParameter("@sequence", SqlDbType.BigInt)
+            var sequenceSqlParameter = new SqlParameter("@sequenceNumber", SqlDbType.BigInt)
             {
                 Direction = ParameterDirection.Output
             };
 
-            await _dbContext.Database.ExecuteSqlRawAsync($"SELECT NEXT VALUE FOR {sequenceName}", sequenceSqlParameter);
+            await _dbContext.Database.ExecuteSqlRawAsync($"set @sequenceNumber = NEXT VALUE FOR {sequenceName}", sequenceSqlParameter);
 
             return (long)sequenceSqlParameter.Value;
         }
