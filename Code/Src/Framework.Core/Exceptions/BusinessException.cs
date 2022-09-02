5c5e8a33deb752c7d798d6048cc2e337cf21200d
Pakan <Pakan@DESKTOP-KNI3379>
2022-09-02 15:32:59 +0430

    add EntityNotFoundException to framework.core

--- Code/Src/Framework.Core/Exceptions/BusinessException.cs
+++ Code/Src/Framework.Core/Exceptions/BusinessException.cs
@@ -1,41 +1,44 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace Framework.Core.Exceptions
 {
     public class BusinessException : Exception
     {
         private const long BoundedContextCodesDefault = -1000;
-        #region Properties
-
         public long ErrorCode { get; private set; }
         public string ExceptionMessage { get; private set; }
-        public long BoundedContextCode { get; private set; } 
-
-        #endregion
-
-        #region Constructors
-
+        public long BoundedContextCode { get; private set; }
         protected BusinessException() { }
-        public BusinessException(long code,
-            string message,
-            long boundedContextCode = BoundedContextCodesDefault)
-        {
-            this.ErrorCode = code;
-            this.ExceptionMessage = message;
-            this.BoundedContextCode = boundedContextCode;
-        }
 
-        public BusinessException(Enum errorCode,
-            string errorMessage,
-            long boundedContextCode = BoundedContextCodesDefault)
+        public BusinessException(string message)
+        {
+            this.ErrorCode = -1;
+            this.ExceptionMessage = message;
+            this.BoundedContextCode = BoundedContextCodesDefault;
+        }
+        public BusinessException(long errorCode, string message)
+        {
+            this.ErrorCode = errorCode;
+            this.ExceptionMessage = message;
+            this.BoundedContextCode = BoundedContextCodesDefault;
+        }
+        public BusinessException(Enum errorCode, string errorMessage)
+        {
+            this.ErrorCode = Convert.ToInt32(errorCode);
+            this.ExceptionMessage = errorMessage;
+            this.BoundedContextCode = BoundedContextCodesDefault;
+        }
+        public BusinessException(Enum errorCode, string errorMessage, long boundedContextCode)
         {
             this.ErrorCode = Convert.ToInt32(errorCode);
             this.ExceptionMessage = errorMessage;
             this.BoundedContextCode = boundedContextCode;
         }
-
-        #endregion
+        public BusinessException(long errorCode, string errorMessage, long boundedContextCode)
+        {
+            this.ErrorCode = errorCode;
+            this.ExceptionMessage = errorMessage;
+            this.BoundedContextCode = boundedContextCode;
+        }
     }
 }
---
+++ Code/Src/Framework.Core/Exceptions/EntityNotFoundException.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Framework.Core.Exceptions
+{
+    public class EntityNotFoundException<TKey> : BusinessException
+    {
+        public EntityNotFoundException(string entityName, TKey id) : base($"Entity '{entityName}' not found with Id : {id}")
+        {
+        }
+        public EntityNotFoundException(long boundedContextCode, TKey id) : base(-1, $"Entity not found with Id : {id}", boundedContextCode)
+        {
+
+        }
+        public EntityNotFoundException(long boundedContextCode, string entityName, TKey id) : base(-1, $"Entity '{entityName}' not found with Id : {id}", boundedContextCode)
+        {
+        }
+        public EntityNotFoundException(TKey id) : base($"Entity not found with Id : {id}")
+        {
+
+        }
+
+        public EntityNotFoundException(string entityName, TKey id, long errorCode) : base(errorCode, $"Entity '{entityName}' not found with Id : {id}")
+        {
+        }
+        public EntityNotFoundException(string entityName, TKey id, Enum errorCode) : base(errorCode, $"Entity '{entityName}' not found with Id : {id}")
+        {
+        }
+        public EntityNotFoundException(long boundedContextCode, string entityName, TKey id, long errorCode) : base(errorCode, $"Entity '{entityName}' not found with Id : {id}", boundedContextCode)
+        {
+        }
+        public EntityNotFoundException(long boundedContextCode, string entityName, TKey id, Enum errorCode) : base(errorCode, $"Entity '{entityName}' not found with Id : {id}", boundedContextCode)
+        {
+        }
+    }
+}
