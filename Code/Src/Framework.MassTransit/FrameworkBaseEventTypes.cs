234fb772f02dc93c35d5d6f218c9ae1faa241065
Pakan <Pakan@DESKTOP-KNI3379>
2022-09-26 20:50:14 +0330

    add factory method for Create message From MassTransit Context on Ievent

---
+++ Code/Src/Framework.MassTransit/FrameworkBaseEventTypes.cs
@@ -0,0 +1,20 @@
+﻿using Framework.Core.Events;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace Framework.MassTransit
+{
+    public static class FrameworkBaseEventTypes
+	{
+		public static IReadOnlyCollection<string> EventTypeNames { get; }
+		static FrameworkBaseEventTypes()
+		{
+            EventTypeNames = new ReadOnlyCollection<string>(new List<string>
+			{
+				   $"urn:message:{typeof(IDomainEvent).Namespace}:{nameof(IDomainEvent)}",
+				   $"urn:message:{typeof(IEvent).Namespace}:{nameof(IEvent)}",
+				   $"urn:message:{typeof(DomainEvent).Namespace}:{nameof(DomainEvent)}",
+			});
+		}
+	}
+}
--- Code/Src/Framework.MassTransit/MassTransitMessageFactory.cs
+++ Code/Src/Framework.MassTransit/MassTransitMessageFactory.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Framework.Core.Events;
 using Framework.Messages;
 using MassTransit;
 using Newtonsoft.Json;
@@ -9,22 +10,37 @@ namespace Framework.MassTransit
 {
 	public class MassTransitMessageFactory
 	{
+        public static MassTransitMessage CreateFromMassTransitContext(ConsumeContext<IEvent> context)
+        {
+            var message = GetMessage(context);
+            message.MessageType = RemoveBaseTypesForEvent(message.MessageType);
+            message.MessageType = ConvertTypesToNamespaceNames(message.MessageType);
+            return message;
+        }
 		public static MassTransitMessage CreateFromMassTransitContext(ConsumeContext<IMessage> context)
 		{
 			var message = GetMessage(context);
-			message.MessageType = RemoveBaseTypes(message.MessageType);
+			message.MessageType = RemoveBaseTypesForMessage(message.MessageType);
 			message.MessageType = ConvertTypesToNamespaceNames(message.MessageType);
 			return message;
 		}
-
+        private static MassTransitMessage GetMessage(ConsumeContext<IEvent> context)
+        {
+            var body = context.ReceiveContext.GetBody();
+            return JsonConvert.DeserializeObject<MassTransitMessage>(Encoding.UTF8.GetString(body));
+        }
 		private static MassTransitMessage GetMessage(ConsumeContext<IMessage> context)
 		{
 			var body = context.ReceiveContext.GetBody();
 			return JsonConvert.DeserializeObject<MassTransitMessage>(Encoding.UTF8.GetString(body));
 		}
-		private static IList<string> RemoveBaseTypes(IList<string> messageMessageType)
+        private static IList<string> RemoveBaseTypesForMessage(IList<string> messageMessageType)
+        {
+            return messageMessageType.Where(a => !BaseEventTypes.TypeNames.Any(b => b.Equals(a))).ToList();
+        }
+		private static IList<string> RemoveBaseTypesForEvent(IList<string> messageMessageType)
 		{
-			return messageMessageType.Where(a => !BaseEventTypes.TypeNames.Any(b => b.Equals(a))).ToList();
+			return messageMessageType.Where(a => !FrameworkBaseEventTypes.EventTypeNames.Any(b => b.Equals(a))).ToList();
 		}
 		private static IList<string> ConvertTypesToNamespaceNames(IList<string> messageTypes)
 		{
