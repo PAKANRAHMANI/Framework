c27f77106e82f6ba7bc478f591b825be2a80f5a7
e.faridi <ehsanfaridi1992@gmail.com>
2022-10-18 17:46:51 +0330

    add pagination

---
+++ Code/Src/Framework.Core/Pagination/PaginationHelper.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Framework.Core.Pagination
+{
+    public static class PaginationHelper
+    {
+        public static List<T> ApplyPagination<T>(this List<T> models, PaginationModel paginationModel)
+        {
+            var pageIndex = paginationModel.PageIndex <= 0 ? 0 : paginationModel.PageIndex - 1;
+
+            var pageSize = paginationModel.PageSize == 0 ? int.MaxValue : paginationModel.PageSize;
+
+            paginationModel.TotalCount = models.Count;
+
+            var pageCount = models.Count / pageSize;
+            if (pageIndex > pageCount)
+                pageIndex = 1;
+
+            paginationModel.TotalPageCount = pageCount == 0 ? 1 : pageCount;
+
+            var result = models
+                .Skip(pageIndex * paginationModel.PageSize)
+                .Take(pageSize).ToList();
+            return result;
+        } 
+    }
+}
---
+++ Code/Src/Framework.Core/Pagination/PaginationModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Framework.Core.Pagination;
+
+public abstract class PaginationModel
+{
+    public int PageIndex { get; set; }
+    public int PageSize { get; set; }
+    public int TotalCount { get; set; }
+    public int TotalPageCount { get; set; }
+}
