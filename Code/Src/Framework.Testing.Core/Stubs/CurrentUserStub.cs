7de8ffc3600d11081c097781182fa83db73a16df
Pakan <Pakan@DESKTOP-KNI3379>
2023-07-29 18:48:23 +0330

    fix stub for icurrent

--- Code/Src/Framework.Testing.Core/Stubs/CurrentUserStub.cs
+++ Code/Src/Framework.Testing.Core/Stubs/CurrentUserStub.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Security.Claims;
 using Framework.Core;
 using Framework.Testing.Core.Authentication;
@@ -17,6 +19,42 @@ namespace Framework.Testing.Core.Stubs
             };
             var claimsIdentity = new ClaimsIdentity(claims);
             return new TestPrincipal(claimsIdentity);
+        }
+
+        public TKey GetUserIdFromNameIdentifier<TKey>()
+        {
+            var claims = new List<Claim>()
+            {
+                new Claim(ClaimTypes.NameIdentifier,"1"),
+                new Claim(ClaimTypes.Email,"pakan.rahmani@gmail.com"),
+                new Claim(ClaimTypes.Name,"pakan rahmani"),
+            };
+
+            var claimsIdentity = new ClaimsIdentity(claims);
+
+            var principal = new TestPrincipal(claimsIdentity);
+
+            var sub = principal.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier)?.Value;
+
+            return (TKey)Convert.ChangeType(sub, typeof(TKey));
+        }
+
+        public TKey GetUserIdFromSub<TKey>()
+        {
+            var claims = new List<Claim>()
+            {
+                new Claim(ClaimTypes.NameIdentifier,"1"),
+                new Claim(ClaimTypes.Email,"pakan.rahmani@gmail.com"),
+                new Claim(ClaimTypes.Name,"pakan rahmani"),
+            };
+
+            var claimsIdentity = new ClaimsIdentity(claims);
+
+            var principal = new TestPrincipal(claimsIdentity);
+
+            var sub = principal.Claims.FirstOrDefault(c => c.Type == "sub")?.Value;
+
+            return (TKey)Convert.ChangeType(sub, typeof(TKey));
         }
     }
 }
