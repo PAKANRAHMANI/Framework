b7303d977534ff36aade71a8264a9abae4b7d36b
e.faridi <e.faridi@PC-E-FARIDI>
2023-07-31 09:37:08 +0330

    adding MessageTypeString

--- Code/Src/Framework.Messages/IMessage.cs
+++ Code/Src/Framework.Messages/IMessage.cs
@@ -5,6 +5,7 @@ namespace Framework.Messages
     public interface IMessage
     {
         Type MessageType { get; }
+        string MessageTypeString { get; }
         Guid MessageId { get; }
         DateTime PublishDateTime { get; }
         public string Body { get; set; }
--- Code/Src/Framework.Messages/Message.cs
+++ Code/Src/Framework.Messages/Message.cs
@@ -5,6 +5,7 @@ namespace Framework.Messages
     public class Message : IMessage
     {
         public Type MessageType { get; set; }
+        public string MessageTypeString { get; set; }
         public Guid MessageId { get; protected set; }
         public DateTime PublishDateTime { get; protected set; }
         public string Body { get; set; }
