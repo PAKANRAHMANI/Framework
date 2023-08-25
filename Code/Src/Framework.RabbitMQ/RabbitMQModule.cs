f20e0ba13c01fb43e813d87d3aa4ec283515e9f2
Pakan <Pakan@DESKTOP-KNI3379>
2023-08-25 14:56:28 +0330

    add RabbitMQ Module

---
+++ Code/Src/Framework.RabbitMQ/RabbitMQModule.cs
@@ -0,0 +1,26 @@
+﻿using Framework.Config;
+using System;
+
+namespace Framework.RabbitMQ
+{
+    public class RabbitMQModule : IFrameworkModule
+    {
+        private readonly RabbitConfiguration _config;
+
+        public RabbitMQModule(Action<RabbitConfiguration>  configuration)
+        {
+            _config = new RabbitConfiguration();
+
+            configuration.Invoke(_config);
+        }
+
+        public void Register(IDependencyRegister dependencyRegister)
+        {
+            dependencyRegister.RegisterSingleton(typeof(RabbitConfiguration), _config);
+
+            dependencyRegister.RegisterSingleton<IAcknowledgeManagement, AcknowledgeManagement>();
+
+            dependencyRegister.RegisterSingleton<IRabbitMqMessageSender, RabbitMqMessageSender>();
+        }
+    }
+}
