495d040cb32b2a1b6451bccb4289ac85da368844
e.faridi <e.faridi@PC-E-FARIDI>
2023-10-07 10:53:00 +0330

    adding concreate class for Event

---
+++ Code/Src/Framework.Core/Events/Event.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Framework.Core.Events;
+
+public abstract class Event : IEvent
+{
+    public Guid EventId { get; protected set; }
+    public DateTime PublishDateTime { get; protected set; }
+
+    protected Event()
+    {
+        this.EventId = Guid.NewGuid();
+        this.PublishDateTime = DateTime.UtcNow;
+    }
+}
