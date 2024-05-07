a974df9f3ee11ae6b2731de7e3da0dc27bd050b3
Pakan <Pakan@DESKTOP-KNI3379>
2024-05-07 20:21:04 +0330

    rename EfPersistTest to EfPersistTestDbContext and add ApplyConfigurationsFromAssembly

--- Code/Src/Framework.Testing.Core/Persist/EfPersistTestDbContext.cs
+++ Code/Src/Framework.Testing.Core/Persist/EfPersistTestDbContext.cs
@@ -1,28 +1,36 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Transactions;
+using System.Reflection;
 using Framework.DataAccess.EF;
 using Framework.Testing.Core.Fakes;
 using Microsoft.EntityFrameworkCore;
 
 namespace Framework.Testing.Core.Persist
 {
-    public abstract class EfPersistTest<TDbContext> : IDisposable where TDbContext : FrameworkDbContext
+    public abstract class EfPersistTestDbContext<TDbContext> : FrameworkDbContext, IDisposable where TDbContext : FrameworkDbContext
     {
+        private readonly Assembly _mappingAssembly;
         public readonly EfUnitOfWork EfUnitOfWork;
         public TDbContext DbContext;
         public FakeConfigurator Configurator { get; private set; }
-        protected EfPersistTest(string connectionString)
+        protected EfPersistTestDbContext(string connectionString, Assembly mappingAssembly)
         {
-            this.Configurator = new FakeConfigurator();
-            var option = new DbContextOptionsBuilder().UseSqlServer(connectionString);
-            this.DbContext = (TDbContext)Activator.CreateInstance(typeof(TDbContext), option.Options);
-            this.EfUnitOfWork = new EfUnitOfWork(this.DbContext);
-        }
+            this._mappingAssembly = mappingAssembly;
 
+            this.Configurator = new FakeConfigurator();
+
+            var option = new DbContextOptionsBuilder().UseSqlServer(connectionString);
+
+            this.DbContext = (TDbContext)Activator.CreateInstance(typeof(TDbContext), option.Options);
+
+            this.EfUnitOfWork = new EfUnitOfWork(this.DbContext);
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(_mappingAssembly);
+
+            base.OnModelCreating(modelBuilder);
+        }
         public void Dispose()
         {
             this.DbContext?.Dispose();
