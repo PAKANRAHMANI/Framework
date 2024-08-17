7aa369eb1707f9deb503e6eec7793af1bc6f7813
m.mohammadii <mimohammadi72@gmail.com>
2024-08-17 12:06:49 +0330

    fix bug in register sqlDAtaStor config

--- Code/Src/Framework.EventProcessor/DataStore/Sql/SqlUpdateCursorPosition.cs
+++ Code/Src/Framework.EventProcessor/DataStore/Sql/SqlUpdateCursorPosition.cs
@@ -1,17 +1,26 @@
 ﻿using Framework.Core.Logging;
 using Framework.EventProcessor.Configurations;
 using Framework.EventProcessor.DataStore.ChangeTrackers;
+using Microsoft.Extensions.Options;
 
 namespace Framework.EventProcessor.DataStore.Sql;
 
-public class SqlUpdateCursorPosition(SqlStoreConfig sqlStoreConfig, ILogger logger) : IUpdateCursorPosition
+public class SqlUpdateCursorPosition: IUpdateCursorPosition
 {
+    private readonly SqlStoreConfig _sqlStoreConfig;
+    private readonly ILogger _logger;
+
+    public SqlUpdateCursorPosition(IOptions<SqlStoreConfig> sqlStoreConfig, ILogger logger)
+    {
+        _sqlStoreConfig = sqlStoreConfig.Value;
+        _logger = logger;
+    }
     public void MoveCursorPosition(EventItem eventItem)
     {
-        SqlQueries.MoveCursorPosition(sqlStoreConfig.ConnectionString, eventItem.Id,
-            sqlStoreConfig.CursorTable);
+        SqlQueries.MoveCursorPosition(_sqlStoreConfig.ConnectionString, eventItem.Id,
+            _sqlStoreConfig.CursorTable);
 
-        logger.Write($"cursor moved to : {eventItem.Id}", LogLevel.Information);
+        _logger.Write($"cursor moved to : {eventItem.Id}", LogLevel.Information);
 
     }
 }
