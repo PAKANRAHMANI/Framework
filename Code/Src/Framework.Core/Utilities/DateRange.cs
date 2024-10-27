b7abdefd1884035e70a8e02e51fa1d5503213464
z.daneshi <z.daneshi@charisma.tech>
2024-10-27 15:15:05 +0330

    #add date time range

--- Code/Src/Framework.Core/Utilities/DateRange.cs
+++ Code/Src/Framework.Core/Utilities/DateRange.cs
@@ -2,30 +2,70 @@
 using System.Collections.Generic;
 using Framework.Core.Exceptions;
 
-namespace Framework.Core.Utilities
+namespace Framework.Core.Utilities;
+
+public struct DateRange : IEqualityComparer<DateRange>
 {
-    public struct DateRange : IEqualityComparer<DateRange>
+    public DateOnly StartDate { get; private set; }
+
+    public DateOnly EndDate { get; private set; }
+    public DateRange(DateOnly startDate, DateOnly endDate)
     {
-        public DateTime StartDate { get; private set; }
+        if (startDate >= endDate)
+            throw new EndDateShouldBeGreaterThanStartDateException();
 
-        public DateTime EndDate { get; private set; }
-        public DateRange(DateTime startDate, DateTime endDate)
-        {
-            if (startDate >= endDate)
-                throw new EndDateShouldBeGreaterThanStartDateException();
-
-            this.StartDate = startDate;
-            this.EndDate = endDate;
-        }
-        public bool Includes(DateTime value) => value >= StartDate && value <= EndDate;
-        public bool Equals(DateRange x, DateRange y)
-        {
-            return x.StartDate.Equals(y.StartDate) && x.EndDate.Equals(y.EndDate);
-        }
-
-        public int GetHashCode(DateRange obj)
-        {
-            return HashCode.Combine(obj.StartDate, obj.EndDate);
-        }
+        this.StartDate = startDate;
+        this.EndDate = endDate;
     }
+    public bool Includes(DateOnly value) => value >= StartDate && value <= EndDate;
+    public bool Equals(DateRange x, DateRange y)
+    {
+        return x.StartDate.Equals(y.StartDate) && x.EndDate.Equals(y.EndDate);
+    }
+
+    public int GetHashCode(DateRange obj)
+    {
+        return HashCode.Combine(obj.StartDate, obj.EndDate);
+    }
+    public override string ToString()
+    {
+        return $"{StartDate:yy-MM-dd} - {EndDate:yy-MM-dd}";
+    }
+
+    public static implicit operator string(DateRange timeRange)
+    {
+        return timeRange.ToString();
+    }
+
+    public static bool operator ==(DateRange firstDate, DateRange secondDate)
+    {
+        return firstDate != null && secondDate != null && (firstDate == secondDate);
+    }
+
+    public static bool operator !=(DateRange firstDate, DateRange secondDate)
+    {
+        return !(firstDate == secondDate);
+    }
+
+    public static bool operator <=(DateRange firstDate, DateRange secondDate)
+    {
+        return firstDate != null && firstDate <= secondDate;
+    }
+
+    public static bool operator >=(DateRange firstDate, DateRange secondDate)
+    {
+        return firstDate != null && firstDate >= secondDate;
+    }
+
+    public static bool operator >(DateRange firstDate, DateRange secondDate)
+    {
+        return firstDate > secondDate;
+    }
+
+    public static bool operator <(DateRange firstDate, DateRange secondDate)
+    {
+        return firstDate < secondDate;
+    }
+
+
 }
---
+++ Code/Src/Framework.Core/Utilities/DateTimeRange.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using Framework.Core.Exceptions;
+
+namespace Framework.Core.Utilities;
+
+public struct DateTimeRange : IEqualityComparer<DateTimeRange>
+{
+    public DateTime StartDateTime { get; private set; }
+    public DateTime EndDateTime { get; private set; }
+
+    public DateTimeRange(DateTime startDateTime, DateTime endDateTime)
+    {
+        if (startDateTime >= endDateTime)
+            throw new EndDateShouldBeGreaterThanStartDateException();
+
+        this.StartDateTime = startDateTime;
+        this.EndDateTime = endDateTime;
+    }
+
+    public bool Includes(DateTime value) => value >= StartDateTime && value <= EndDateTime;
+    public bool Equals(DateTimeRange x, DateTimeRange y) => x.StartDateTime.Equals(y.StartDateTime) && x.EndDateTime.Equals(y.EndDateTime);
+    public int GetHashCode(DateTimeRange obj) => HashCode.Combine(obj.StartDateTime, obj.EndDateTime);
+
+    public override string ToString() => $"{StartDateTime:O} - {EndDateTime:O}";
+    public static implicit operator string(DateTimeRange timeRange) => timeRange.ToString();
+
+    public static bool operator ==(DateTimeRange firstTime, DateTimeRange secondTime) => firstTime != null && secondTime != null && (firstTime == secondTime);
+    public static bool operator !=(DateTimeRange firstTime, DateTimeRange secondTime) => !(firstTime == secondTime);
+    public static bool operator <=(DateTimeRange firstTime, DateTimeRange secondTime) => firstTime != null && firstTime <= secondTime;
+    public static bool operator >=(DateTimeRange firstTime, DateTimeRange secondTime) => firstTime != null && firstTime >= secondTime;
+    public static bool operator >(DateTimeRange firstTime, DateTimeRange secondTime) => firstTime > secondTime;
+    public static bool operator <(DateTimeRange firstTime, DateTimeRange secondTime) => firstTime < secondTime;
+
+}
