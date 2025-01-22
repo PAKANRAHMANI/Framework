68c87d4211862edf64d01aa71964353b716a2038
f.manafi <f.manafi@NC-F-MANAFI>
2025-01-22 12:41:42 +0330

    Move RateLimit Project to AspNetCore Project

---
+++ Code/Src/Framework.AspNetCore/Configurations/RateLimitSetting.cs
@@ -0,0 +1,12 @@
+﻿namespace Framework.AspNetCore.Configurations
+{
+    public class RateLimitSetting
+    {
+        public int PermitLimit { get; set; }
+        public int Window { get; set; }
+        public int QueueLimit { get; set; }
+        public int StatusCode { get; set; }
+        public int QueueProcessingOrder { get; set; } = (int)System.Threading.RateLimiting.QueueProcessingOrder.OldestFirst;
+        public bool AutoReplenishment { get; set; } = true;
+    }
+}
