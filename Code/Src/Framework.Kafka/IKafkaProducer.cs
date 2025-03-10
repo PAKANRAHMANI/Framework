71939ee3f484e83739409c707398cee281ce5c17
f.manafi <f.manafi@NC-F-MANAFI>
2025-03-10 09:38:28 +0330

    Add Kafka Produce by Header by topic name

--- Code/Src/Framework.Kafka/IKafkaProducer.cs
+++ Code/Src/Framework.Kafka/IKafkaProducer.cs
@@ -13,7 +13,8 @@ namespace Framework.Kafka
         Task<DeliveryResult<TKey, TMessage>> ProduceAsync(TKey key, TMessage message, KeyValuePair<string, string>[] headers, CancellationToken cancellationToken = default, string eventId = null);
         Task<DeliveryResult<TKey, TMessage>> ProduceAsync(TKey key, TMessage message, int partitionNumber, CancellationToken cancellationToken = default, string eventId = null);
         void Produce(TKey key, TMessage message, Action<DeliveryResult<TKey, TMessage>> action = null, string eventId = null);
-        void Produce(TKey key, TMessage message,  Action<DeliveryResult<TKey, TMessage>> action = null, string eventId = null, params KeyValuePair<string, string>[] headers);
+        void Produce(TKey key, TMessage message, Action<DeliveryResult<TKey, TMessage>> action = null, string eventId = null, params KeyValuePair<string, string>[] headers);
+        void Produce(TKey key, TMessage message, string topicName, Action<DeliveryResult<TKey, TMessage>> action = null, string eventId = null, params KeyValuePair<string, string>[] headers);
         void Produce(TKey key, TMessage message, int partitionNumber, Action<DeliveryResult<TKey, TMessage>> action = null, string eventId = null);
         void Produce(TKey key, TMessage message, string topicName, int partitionNumber, Action<DeliveryResult<TKey, TMessage>> action = null, string eventId = null);
         void Produce(TKey key, TMessage message, string topicName, Action<DeliveryResult<TKey, TMessage>> action = null, string eventId = null);
